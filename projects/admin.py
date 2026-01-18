7fdd312a24cc8ec967bb87c2711875a54ad8ba69
PavanYalavarthi <pavan.k.yalavarthi@icloud.com>
2026-01-18 17:56:08 +0530

    Added Review and Tag tables

--- projects/admin.py
+++ projects/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Project
+from .models import Project, Review, Tag
 
-admin.site.register(Project)
+admin.site.register([Project, Review, Tag])
